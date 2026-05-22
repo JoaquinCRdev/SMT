baf2fd0af9b3ea762ec1dece143717b8134f4a07
aimesananton <aimesananton@gmail.com>
2026-05-22 15:14:48 -0300

    UI inicio completo

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import {BrowserRouter, Route, Routes} from "react-router-dom"
+import Inicio from "./pages/inicio"
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/inicio" element={<Inicio />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   )
 }
 
