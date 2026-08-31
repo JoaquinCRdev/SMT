a35567b0814249405b0d57ac71ea08574326f91f
Joaco <jcacerestech@gmail.com>
2026-08-31 16:21:19 -0300

    descarga i18n

--- client/src/i18n/i18n.js
+++ client/src/i18n/i18n.js
@@ -1,11 +1,13 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
 
 import es from "./locales/es.json";
 import en from "./locales/en.json";
 import de from "./locales/de.json";
 
 i18n
+  .use(LanguageDetector)
   .use(initReactI18next)
   .init({
     resources: {
@@ -20,8 +22,12 @@ i18n
       },
     },
 
-    lng: "es",
     fallbackLng: "es",
+
+    detection: {
+      order: ["localStorage", "navigator"],
+      caches: ["localStorage"],
+    },
 
     interpolation: {
       escapeValue: false,
